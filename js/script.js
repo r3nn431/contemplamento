529ba55a344fc47fa7c7d36284e605cd32e1ea3f
r3nn <44883517+r3nn431@users.noreply.github.com>
2021-11-28 14:21:55 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-//Contemplamento
+//Contemplamento - JQuery
+
+$(function(){
+    $('#btn1').click(function(){
+        $('p').fadeOut('slow');
+    });
+});
+
+function saveCookie(){
+    localStorage.setItem("scrl", document.getElementById("#menu").scrollTop);
+}
+
+function restoreCookie(){
+    document.getElementById("#menu").innerHTML = localStorage.getItem("scrl");
+    var scrl = localStorage.getItem("scrl");
+    scrollTop = scrl;
+}
