9619895ca3cc2c0737577a5171f37cca4f9d0d9c
r3nn <44883517+r3nn431@users.noreply.github.com>
2022-02-23 13:31:41 -0300

    Added see

--- js/script.js
+++ js/script.js
@@ -1,38 +1,38 @@
 //Contemplamento - JQuery
 
-$("#readmain, #playmain, #seemain, #remembermain").hide();
+$("#readmain, #playmain, #remembermain").hide();
 
 function toggleTo(){
     if($("#tomain").is(":hidden")){
         $("#tomain").show();
-        $("#readmain, #playmain, #seemain, #remembermain").hide();
+        $("#readmain, #playmain, #remembermain").hide();
     }
 }
 
 function toggleRead(){
     if($("#readmain").is(":hidden")){
         $("#readmain").show();
-        $("#tomain, #playmain, #seemain, #remembermain").hide();
+        $("#tomain, #playmain, #remembermain").hide();
     }
 }
 
 function togglePlay(){
     if($("#playmain").is(":hidden")){
         $("#playmain").show();
-        $("#tomain, #readmain, #seemain, #remembermain").hide();
+        $("#tomain, #readmain, #remembermain").hide();
     }
 }
 
-function toggleSee(){
+/*function toggleSee(){
     if($("#seemain").is(":hidden")){
         $("#seemain").show();
         $("#tomain, #readmain, #playmain, #remembermain").hide();
     }
-}
+}*/
 
 function toggleRemember(){
     if($("#remembermain").is(":hidden")){
         $("#remembermain").show();
-        $("#tomain, #readmain, #playmain, #seemain").hide();
+        $("#tomain, #readmain, #playmain").hide();
     }
 }
